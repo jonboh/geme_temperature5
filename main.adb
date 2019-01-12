2b4165bb1a5973e12329974984d0ad0da0459a0b
Jon <jon.bosque.hernando@gmail.com>
2019-01-12 11:55:06 +0100

    las minute changes

--- main.adb
+++ main.adb
@@ -35,31 +35,33 @@ procedure main is
       end Set;
    end Shared_Float;
 
-   --Instantiate Shared Floats--
+   --Instantiate Shared Floats (protected objects)--
    Temperature: Shared_Float;
    Reference: Shared_Float;
+   FlagRefReach: Shared_Float;
+
+   --Semaphores--
+   ReachRefEvent: aliased Suspension_Object;
 
 -- Task Procedures --
-ReachRefEvent: aliased Suspension_Object;
-
    prev_control_signal: Float:=0.0;
    prev_error: Float:= 0.0;
+   	    --Estas dos variables se encuentran declaradas como globales para
+   	    --que cada vez que se llame a la tarea periodica no se reinicialicen
    procedure ControlAlgo is
-   error: Float;
-   control_signal: Float;
+      error: Float;
+      control_signal: Float;
    begin
       error := Reference.Get - Temperature.Get;
-      --Put("Error: ");
-      --Put(error);
-      --Put(Reference.Get);
-      --Put(Temperature.Get);
-      --Put_Line("");
       control_signal := prev_control_signal + 17.5*error-15.88*prev_error;
       mockup_control.Heat(control_signal);
       prev_error := error;
       prev_control_signal := control_signal;
       if error < Reference.Get*0.1 then
-         Set_True(ReachRefEvent);
+         if FlagRefReach.Get = 0.0 then
+            Set_True(ReachRefEvent);
+            FlagRefReach.Set(1.0);
+      	 end if;
       end if;
    end ControlAlgo;
 
@@ -69,23 +71,17 @@ ReachRefEvent: aliased Suspension_Object;
    end ReadTemp;
 
    procedure IncreaseRef is
-   aux_value: Float;
    begin
-      aux_value := Reference.Get;
-      Reference.Set(aux_value + 2.0);
-      Put("New Reference: ");
-      Put(Reference.Get);
-      Put_Line("");
+      FlagRefReach.Set(0.0);
+      Reference.Set(Reference.Get + 2.0);
+      Put("New Reference: ");Put(Reference.Get);Put_Line("");
    end IncreaseRef;
 
    procedure DecreaseRef is
-   aux_value: Float;
    begin
-      aux_value := Reference.Get;
-      Reference.Set(aux_value - 2.0);
-      Put("New Reference: ");
-      Put(Reference.Get);
-      Put_Line("");
+      FlagRefReach.Set(0.0);
+      Reference.Set(Reference.Get - 2.0);
+      Put("New Reference: ");Put(Reference.Get);Put_Line("");
    end DecreaseRef;
 
    prev_time: Time:= Clock;
@@ -105,12 +101,8 @@ ReachRefEvent: aliased Suspension_Object;
 
 -- Tasks --
    T_Control: pertask_gen.Periodic_Task(1,55,2,500,ControlAlgo'Access);
-   T_ReadTemp: pertask_gen.Periodic_Task(2, 50, 2, 2000, ReadTemp'Access);
-   IncreaseRefEvent: aliased Suspension_Object;
-   T_IncreaseRef: esptask_gen.Esporadic_Task(3, 45, 2,500, IncreaseRef'Access,IncreaseRefEvent'Access);
-   DecreaseRefEvent: aliased Suspension_Object;
-   T_DecreaseRef: esptask_gen.Esporadic_Task(4, 40, 2,500, DecreaseRef'Access,DecreaseRefEvent'Access);
-   T_ReachRef: esptask_gen.Esporadic_Task(5, 35, 5, 500, ShowTemp'Access,ReachRefEvent'Access);
+   T_ReadTemp: pertask_gen.Periodic_Task(2, 40, 2, 2000, ReadTemp'Access);
+   T_ReachRef: esptask_gen.Esporadic_Task(3, 50, 5, 500, ShowTemp'Access,ReachRefEvent'Access);
 
 -- Anonymus Task for Keyboard --
    -- Declaration --
@@ -124,14 +116,12 @@ ReachRefEvent: aliased Suspension_Object;
       loop
          Get(cKeyPress);
          if cKeyPress = 'i' then
-            Set_True(IncreaseRefEvent);
+	    IncreaseRef;
          elsif cKeyPress = 'd' then
-            Set_True(DecreaseRefEvent);
+	    DecreaseRef;
          elsif cKeyPress = 'f' then
             ComienzoFin.Fin.Set_Finish;
             -- Allow Sporadic Task To continue to exit
-            Set_True(DecreaseRefEvent);
-            Set_True(IncreaseRefEvent);
             Set_True(ReachRefEvent);
          end if;
          exit when ComienzoFin.Fin.Get_Finish;
@@ -141,6 +131,7 @@ ReachRefEvent: aliased Suspension_Object;
 
 begin
    Put_Line("All Tasks Active");
+   FlagRefReach.Set(0.0);
    Reference.Set(35.0);
    ComienzoFin.Comienzo.Set_Initial_Time(Clock+Seconds(3));
    delay until clock + seconds(1);
